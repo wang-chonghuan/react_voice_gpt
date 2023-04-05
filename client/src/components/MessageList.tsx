821f591faaa7f54a8d51dbb3488716f899b5d33a
wang-chonghuan <wangchonghuan@gmail.com>
2023-04-05 01:34:40 +0100

    msg bg color

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -1,14 +1,25 @@
-import {MessageBody} from "../api/types";
+import { MessageBody } from "../api/types";
 
 type Props = {
   msgs: MessageBody[];
 };
 export function MessageList({ msgs }: Props) {
 
+  const getListItemStyle = (role: string) => {
+    return role === "user"
+      ? "bg-gray-100 text-black" // 使用淡灰色背景和黑色文字
+      : role === "assistant"
+        ? "bg-white text-black"
+        : "";
+  };
+
   return (
     <ul className="list-none">
       {msgs.map((msgBody) => (
-        <li key={msgBody.datetime} className="border-b py-4">
+        <li
+          key={msgBody.datetime}
+          className={`border-b py-4 ${getListItemStyle(msgBody.role)}`}
+        >
           <h3 className="text-slate-900 font-bold">{msgBody.role}</h3>
           <p className=" text-slate-900 ">{msgBody.content}</p>
         </li>
