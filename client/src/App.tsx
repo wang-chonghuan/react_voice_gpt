341973d451b6742b62905a3e83dfc75603aa3f4b
wang-chonghuan <wangchonghuan@gmail.com>
2023-04-08 18:10:56 +0100

    implemented protected routes, navigate to signin if unauthenticated after start even visit chat page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,33 +2,33 @@ import {ChatPage} from './components/ChatPage';
 import {HeaderBar} from './components/HeaderBar';
 import SpeechTestPage from "./components/SpeechTestPage";
 import SignInPage from "./components/SignInPage";
-import {AppProvider} from "./auth/AppContext";
+import {AppProvider, useAppContext} from "./auth/AppContext";
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navigate to="signin" />
-  },
-  {
-    path: '/signin',
-    element: <SignInPage />
-  },
-  {
-    path: '/chatpage',
-    element: <ChatPage />
-  },
-  {
-    path: '/setting',
-    element: <SpeechTestPage />
-  }
+  { path: '/', element: <Navigate to="signin" /> },
+  { path: '/signin', element: <SignInPage /> },
+  { path: '/chatpage', element: <ProtectedRoute element={<ChatPage />} /> },
+  { path: '/setting', element: <ProtectedRoute element={<SpeechTestPage />} /> },
 ]);
+
+interface ProtectedRouteProps {
+  element: React.ReactElement;
+}
+
+function ProtectedRoute({ element }: ProtectedRouteProps) {
+  const { user } = useAppContext();
+  if (!user) {
+    return <Navigate to="/signin" />;
+  }
+  return element;
+}
 
 function App() {
   return (
     <div className="min-w-[240px] min-h-screen">
+      <HeaderBar />
       <AppProvider>
-        <HeaderBar />
         <RouterProvider router={router} />
       </AppProvider>
     </div>
--- client/src/auth/AppContext.tsx
+++ client/src/auth/AppContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useContext, useReducer, ReactNode} from 'react';
+import {createContext, useContext, useReducer, ReactNode, useState, useEffect} from 'react';
 import {validateTokenOnServer} from "./validateToken";
 
 // 这个类型是用户输入进来的，用户后续的登录
@@ -87,6 +87,12 @@ export function AppProvider({children}: Props) {
   // 把这个dispatch传给AppContext.Provider组件（其实就是上下文组件），这个组件再包裹app原有的根组件
   // 这样，根组件树上的所有组件，都能读取到dispatch函数了，同时也能读到user permission状态了。
   const [{user, permissions, loading}, dispatch] = useReducer(reducer, initialState);
+  //
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    validateAndSetInitialState().then(r => {});
+  }, []);
 
   // 检查localStorage中是否存在jwt和username,必须用localStorage否则标签页关闭，数据就清空了
   // 初始化AppProvider时，到服务端验证jwt是否有效，验证成功以后，页面跳转时就不再到服务端验证了。
@@ -107,9 +113,24 @@ export function AppProvider({children}: Props) {
     } else {
       console.log("AppProvider NO saved user");
     }
+    setIsInitialized(true);
   };
 
-  validateAndSetInitialState();
+  /*
+  当我提到“返回null以确保应用的其他部分不会被渲染，直到初始化完成”时，
+  我指的是当isInitialized为false时，AppProvider组件将不会渲染其子组件。
+  return null;的作用是阻止子组件的渲染，直到初始化完成。
+
+  当validateAndSetInitialState()函数完成执行后，你会更新isInitialized的状态。
+  这将触发AppProvider组件重新渲染。在重新渲染过程中，它将检查isInitialized的值。
+  如果isInitialized现在为true，那么AppProvider组件将正常渲染其子组件。
+
+  这是React中组件更新和重新渲染的基本原理。当组件的状态（如isInitialized）发生变化时，组件会重新渲染。
+  在这种情况下，当isInitialized从false变为true时，组件将从返回null变为正常渲染子组件。
+   */
+  if (!isInitialized) {
+    return null; // 在完成初始化之前，不要渲染应用的其他部分
+  }
 
   return (
     <AppContext.Provider
